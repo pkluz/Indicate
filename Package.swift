66fd5426f733f4a853ee3539340d287980a61831
Philip Kluz <philip.kluz@gmail.com>
2022-07-07 23:03:26 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "Indicate",
+    defaultLocalization: "en",
     products: [
         .library(name: "Indicate", targets: ["Indicate"])
     ],
